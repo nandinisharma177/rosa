0ecdf8620f81afd7dfc3865b254e425901777240
nandinisharma177 <nsharma177@myseneca.ca>
2022-10-31 22:21:43 -0400

    "initial commit "

---
+++ server.js
@@ -0,0 +1,75 @@
+/*********************************************************************************
+ * WEB322 – Assignment 03
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+ * of this assignment has been copied manually or electronically from any other source
+ * (including 3rd party web sites) or distributed to other students. *
+ * Name: Nandini Sharma, Student ID : 127407211, 
+ * ********************************************************************************/
+
+ let express = require("express");
+ let path = require("path");
+ 
+ let officeData = require("./modules/officeData.js");
+ 
+ let HTTP_PORT = process.env.PORT || 8080;
+ let app = express();
+ 
+ app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "/views/home.html"));
+ });
+ 
+ app.get("/parttimer", (req, res) => {
+   officeData
+     .getPartTimers()
+     .then((data) => {
+       res.json(data);
+     })
+     .catch((err) => {
+       res.json({ message: "no results" });
+     });
+ });
+ 
+ app.get("/employee/:num", (req, res) => {
+   officeData
+     .getEmployeeByNum(req.params.num)
+     .then((data) => {
+       res.json(data);
+     })
+     .catch((err) => {
+       res.json({ message: "no results" });
+     });
+ });
+ 
+ app.get("/audio", (req, res) => {
+   res.sendFile(path.join(__dirname, "/views/audio.html"));
+ });
+ 
+ app.get("/video", (req, res) => {
+   res.sendFile(path.join(__dirname, "/views/video.html"));
+ });
+ 
+ app.get("/table", (req, res) => {
+   res.sendFile(path.join(__dirname, "/views/table.html"));
+ });
+ 
+ app.get("/list", (req, res) => {
+   res.sendFile(path.join(__dirname, "/views/list.html"));
+ });
+ app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/storefront", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/storefront.html"));
+});
+
+ app.get("/*", (req, res) => {
+   res.send("404 - Page Not Found");
+ });
+ 
+ officeData.initialize().then(() => {
+   app.listen(HTTP_PORT, () => {
+     console.log("server listening on: " + HTTP_PORT);
+   });
+ });
+ 
